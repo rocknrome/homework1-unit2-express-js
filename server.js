61fa981c5cffca335badae45d0979a108ac3ffa4
Roman Larionov <pcsaveddotcom@gmail.com>
2023-12-16 17:50:43 -0500

    Tip calculator fixed and solved

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 // //  R'N'R   December 12, 2023
 
 // DEPENDENCIES
-<<<<<<< HEAD
 const express = require("express")
 
 const port = 3000;
 
+//Magic 8 ball responces
+const magic8Responses = [
+    "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+    "You may rely on it", "As I see it yes", "Most likely", "Outlook good",
+    "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+    "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+    "Don't count on it", "My reply is no", "My sources say no",
+    "Outlook not so good", "Very doubtful"
+  ];
+
 //MAIN FUNCTIONALITY
 const app = express()
 
-//ROUTES
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-
-
-const express = require('express');
-
-//MAIN FUNCTIONALITY
-const app = express();
 
 //ROUTES
 app.get('/greeting', (req, res) => {            //route 'greeting'
@@ -32,10 +31,12 @@ app.get('/greeting/:name', (req, res) => {
 });
 
 
-app.get('/tip:total/:tipPercentage', (req, res) => {        //tip calculator route with 2 params
+//tip calculator route with 2 params
+app.get('/tip/:total/:tipPercentage', (req, res) => {
     const total = parseFloat(req.params.total);
-    const tipPercentage = parceFloat(req.params.tipPercentage);
-    res.send();
+    const tipPercentage = parseFloat(req.params.tipPercentage);
+    const tipAmount = total*(tipPercentage / 100);
+    res.send(`The tip amount is: ${tipAmount}`);
 });
 
 
